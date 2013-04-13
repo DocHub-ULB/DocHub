396db7a478aedcc5f4c63a09bb6881b590e30510
Titouan Christophe <titouanchristophe@gmail.com>
2013-04-13 16:02:48 +0200

    [fix] Adding DjangoExtensions (useful dev tools) to INSTALLED_APPS

--- www/settings.py
+++ www/settings.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.markup',
+    'django_extensions',
     'fragments',
     'south',
     'calendar',
