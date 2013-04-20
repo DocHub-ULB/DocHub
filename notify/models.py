9e2a59a1055f304f41ec350a4e07e9cf1ca42abe
Titouan Christophe <titouanchristophe@gmail.com>
2013-04-20 03:06:02 +0200

    [add] Brand new notification app

---
+++ notify/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from polydag.models import Node
+from users.models import User
+
+
+# 1. Event occurs in graph
+# 2. Pre notif created
+# 3. Notif daemon match against followers and create notif
+# 4. User get notif and read it
+
+class PreNotification(models.Model):
+    created = models.DateTimeField(auto_now=True)
+    node = models.ForeignKey(Node) # The node that initiated the notif
+    text = models.CharField(max_length=160)
+    delivered = models.BooleanField(default=False)
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User)
+    node = models.ForeignKey(Node) # The effective node followed by user
+    prenotif = models.ForeignKey(PreNotification)
+    read = models.BooleanField(default=False)
+
