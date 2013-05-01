2c50e39d582b10a1547e796bf13b85ccf5194f74
Nikita Marchant <nikita.marchant@gmail.com>
2013-05-01 15:31:07 +0200

    [rm] useless views imported from whiteboard

--- polydag/urls.py
+++ polydag/urls.py
@@ -3,5 +3,4 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     (r'^(\d+)\.?(html|json)?$', 'polydag.views.getNode'),
-    (r'^(\d+)/short\.?(html|json)?$', 'polydag.views.getNodeShort'),
 ) + staticfiles_urlpatterns()
