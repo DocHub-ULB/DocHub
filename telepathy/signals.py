650d1991de39f5a63a68b9ce9400910a2d2cf1f7
Nikita Marchant <nikita.marchant@gmail.com>
2013-09-21 13:47:55 +0200

    [fix] better doc notifications

--- telepathy/signals.py
+++ telepathy/signals.py
@@ -7,16 +7,12 @@ def thread_save(**kwargs):
 
     if kwargs['created']:
         thread = kwargs['instance']
-        #TODO: use truncate instead of str[:N]
         PreNotification.objects.create(
             node=thread,
-            text="Nouvelle discussion: "+thread.name[:50].encode('utf-8')+"...",
+            text="Nouvelle discussion: "+thread.name.encode('utf-8'),
             url=reverse('thread_show', args=[thread.id]),
             user=thread.user.user
         )
-    else:
-        #TODO : maybe send a notif when a post is edited
-        pass
 
 
 def message_save(**kwargs):
@@ -28,15 +24,11 @@ def message_save(**kwargs):
 
     if kwargs['created']:
         if message.previous:
-            #TODO: use truncate instead of str[:N]
             PreNotification.objects.create(
                 node=thread,
                 text="Answer to {} by {}".format(
-                    thread.name[:50].encode('utf-8'), poster.name.encode('utf-8')
+                    thread.name.encode('utf-8'), poster.name.encode('utf-8')
                 ),
                 url=reverse('thread_show', args=[thread.id]),
                 user=message.user.user
             )
-    else:
-        #TODO : maybe send a notif when a message is edited
-        pass
