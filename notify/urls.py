e4736fdfd3c6fdac196a337265882ec77a59499c
Nikita Marchant <nikita.marchant@gmail.com>
2013-09-25 14:32:54 +0200

    [enh] add a "mark as read" button in notifications

--- notify/urls.py
+++ notify/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^show$', 'notify.views.notifications_show', name="notif_show"),
+    url(r'^read_all$', 'notify.views.notification_read_all', name="notif_read_all"),
     url(r'^(?P<id>[^/]*)/read$', 'notify.views.notification_read', name="notif_read"),
     url(r'^(?P<id>[^/]*)/ajax_read$', 'notify.views.notification_ajax_read', name="notif_ajax_read")
 )
