2da4d328bd4257b54e711c79ec3d670db712eed6
Nikita Marchant <nikita.marchant@gmail.com>
2014-04-09 12:12:25 +0200

    Fix bug with unicode and structs introduced by last commit

--- documents/helpers.py
+++ documents/helpers.py
@@ -32,12 +32,12 @@ def get_image_size(filename):
     if len(head) != 24:
         raise ValueError('{} is less than 24 bytes, cannot be a valid image.'.format(filename))
     if imghdr.what(filename) == 'png':
-        check = struct.unpack('>i', head[4:8])[0]
+        check = struct.unpack(str('>i'), head[4:8])[0]
         if check != 0x0d0a1a0a:
             raise ValueError('{} is not a valid png file.'.format(filename))
-        width, height = struct.unpack('>ii', head[16:24])
+        width, height = struct.unpack(str('>ii'), head[16:24])
     elif imghdr.what(filename) == 'gif':
-        width, height = struct.unpack('<HH', head[6:10])
+        width, height = struct.unpack(str('<HH'), head[6:10])
     elif imghdr.what(filename) == 'jpeg':
         try:
             fhandle.seek(0)  # Read 0xff next
@@ -49,10 +49,10 @@ def get_image_size(filename):
                 while ord(byte) == 0xff:
                     byte = fhandle.read(1)
                 ftype = ord(byte)
-                size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                size = struct.unpack(str('>H'), fhandle.read(2))[0] - 2
             # We are at a SOFn block
             fhandle.seek(1, 1)  # Skip `precision' byte.
-            height, width = struct.unpack('>HH', fhandle.read(4))
+            height, width = struct.unpack(str('>HH'), fhandle.read(4))
         except:
             raise ValueError('{} is not a valid jpeg file.'.format(filename))
     else:
