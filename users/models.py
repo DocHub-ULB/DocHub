fc1c4486d922e6fdea01bd497dd641317ec40ccd
Nikita Marchant <nikita.marchant@gmail.com>
2014-04-19 20:00:04 +0200

    Fix again the default profile pic

--- users/models.py
+++ users/models.py
@@ -48,7 +48,7 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'netid'
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
-    DEFAULT_PHOTO = join(settings.MEDIA_URL, "profile/default.jpg")
+    DEFAULT_PHOTO = join(settings.STATIC_URL, "images/default.jpg")
     objects = CustomUserManager()
 
     netid = models.CharField(max_length=20, unique=True, null=False, blank=False)
