b81c83c393ee247d6d2a838320d51b551b8dfa27
Nikita Marchant <nikita.marchant@gmail.com>
2014-12-01 17:07:24 +0100

    Add error codes to error pagesy

--- www/error.py
+++ www/error.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def error400(request):
-    return render(request, "error/400.html")
+    return render(request, "error/400.html", status=400)
 def error403(request):
-    return render(request, "error/403.html")
+    return render(request, "error/403.html", status=403)
 def error404(request):
-    return render(request, "error/404.html")
+    return render(request, "error/404.html", status=404)
 def error500(request):
-    return render(request, "error/500.html")
+    return render(request, "error/500.html", status=500)
