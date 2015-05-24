982eb1801aab137721e60948771dae776be1d520
Nikita Marchant <nikita.marchant@gmail.com>
2015-05-24 11:45:50 +0200

    Do not evaluate the page_set QuerySet for the viewer and cache the page layout in the viewer.html

--- documents/views.py
+++ documents/views.py
@@ -146,7 +146,7 @@ def document_show(request, id):
         "object": document,
         "parent": document.parent,
         "is_moderator": request.user.is_moderator(document.parent),
-        "page_set": list(document.page_set.order_by('id').all()),
+        "page_set": document.page_set.order_by('id'),
     }
     document.views = F('views') + 1
     document.save(update_fields=['views'])
