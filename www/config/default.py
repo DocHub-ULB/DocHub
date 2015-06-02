838f13d1dc3031790b105839245a428778107cee
Nikita Marchant <nikita.marchant@gmail.com>
2015-06-02 16:02:24 +0200

    Add pagination to the API

--- www/config/default.py
+++ www/config/default.py
@@ -97,9 +97,9 @@ COMPRESS_OFFLINE = True
 
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
 }
