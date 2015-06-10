30d963b684392f34cc8774922962c822c5d54f2c
Nikita Marchant <nikita.marchant@gmail.com>
2015-06-10 10:11:25 +0200

    Ability to restart celery workers

--- fabfile.py
+++ fabfile.py
@@ -14,3 +14,13 @@ def deploy():
         run("./manage.py collectstatic --noinput -v 0")
         run("./manage.py migrate")
         run('sudo supervisorctl start b402-gunicorn')
+
+
+def restart_workers():
+    run('sudo supervisorctl stop  b402-worker1')
+    run('sudo supervisorctl stop  b402-worker2')
+    run('sudo supervisorctl stop  b402-worker3')
+
+    run('sudo supervisorctl start b402-worker3')
+    run('sudo supervisorctl start b402-worker2')
+    run('sudo supervisorctl start b402-worker1')
