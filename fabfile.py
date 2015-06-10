52ed326a8fc8911eafcea8f9b4159db52d04f480
Nikita Marchant <nikita.marchant@gmail.com>
2015-06-10 09:57:03 +0200

    Add a fabfile for easy fabric deployment :)

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from __future__ import with_statement
+from fabric.api import run, cd
+from fabric.context_managers import prefix
+
+
+def deploy():
+    code_dir = '/home/b402/beta402'
+    with cd(code_dir), prefix('source ve/bin/activate'):
+        run('sudo supervisorctl stop b402-gunicorn')
+        run("./save_db.sh")
+        run("git pull")
+        run("pip install -r requirements.txt --upgrade -q")
+        run("./manage.py compress -v 0")
+        run("./manage.py collectstatic --noinput -v 0")
+        run("./manage.py migrate")
+        run('sudo supervisorctl start b402-gunicorn')
