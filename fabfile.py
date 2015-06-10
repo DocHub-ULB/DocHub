157e428791e6766ed3eb8005b5b0e50fd9771291
Nikita Marchant <nikita.marchant@gmail.com>
2015-06-10 10:19:31 +0200

    Add stats to the fabfile

--- fabfile.py
+++ fabfile.py
@@ -24,3 +24,9 @@ def restart_workers():
     run('sudo supervisorctl start b402-worker3')
     run('sudo supervisorctl start b402-worker2')
     run('sudo supervisorctl start b402-worker1')
+
+
+def stats():
+    code_dir = '/home/b402/beta402'
+    with cd(code_dir), prefix('source ve/bin/activate'):
+        run('./manage.py stats')
