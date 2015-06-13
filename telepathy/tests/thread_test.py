749924b3d47a14eae8584d243e2642f4aa36330e
Nikita Marchant <nikita.marchant@gmail.com>
2015-06-13 17:18:41 +0200

    Test thread model

---
+++ telepathy/tests/thread_test.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import pytest
+from telepathy.models import Thread
+from users.models import User
+from catalog.models import Course
+import json
+
+pytestmark = pytest.mark.django_db
+
+
+def create_thread(name):
+    course = Course.objects.create(slug='AAA-000')
+    user = User.objects.create(netid='test_user')
+    thread = Thread.objects.create(name=name, user=user, course=course)
+
+    return thread
+
+
+def test_page_no_none():
+    thread = create_thread('coucou')
+    assert thread.page_no is None
+
+
+def test_page_no():
+    thread = create_thread('coucou')
+    thread.placement = json.dumps({'page-no': 100})
+    assert thread.page_no is 100
