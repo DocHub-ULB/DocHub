1c1981c55a19f832f245635d5b1f1297170e583a
Nikita Marchant <nikita.marchant@gmail.com>
2015-06-14 11:05:04 +0200

    Test documents.tasks.checksum

--- documents/tests/celery_test.py
+++ documents/tests/celery_test.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from documents.models import Document
 from users.models import User
 from documents.tasks import process_document
+from documents import tasks
 from django.core.files import File
 import celery
 from subprocess import call
@@ -36,17 +37,22 @@ def start_unoconv():
     signal.alarm(0) # cancel alarm
 
 
-@pytest.mark.slow
-def test_add_to_queue():
-    user = User.objects.create(netid='test_user')
-
+def create_doc(name, ext):
+    user = User.objects.get_or_create(netid='test_user')[0]
     doc = Document.objects.create(
         user=user,
-        name="Document name",
+        name=name,
         state="IN_QUEUE",
-        file_type='.pdf'
+        file_type=ext
     )
 
+    return doc
+
+
+@pytest.mark.slow
+def test_add_to_queue():
+
+    doc = create_doc("Document name", ".pdf")
     f = File(open('documents/tests/files/3pages.pdf'))
     doc.original.save("silly-unique-deadbeef-file.pdf", f)
 
@@ -65,14 +71,7 @@ def test_add_to_queue():
 def test_send_duplicate():
     test_add_to_queue()
 
-    user = User.objects.create(netid='test_user2', email="lol@lol.com")
-
-    doc = Document.objects.create(
-        user=user,
-        name="Document name2",
-        state="IN_QUEUE",
-        file_type='.pdf'
-    )
+    doc = create_doc("Document name2", ".pdf")
 
     f = File(open('documents/tests/files/3pages.pdf'))
     doc.original.save("another-uuid-beef-dead.pdf", f)
@@ -87,14 +86,7 @@ def test_send_duplicate():
 @pytest.mark.unoconv
 @pytest.mark.slow
 def test_send_office():
-    user = User.objects.create(netid='test_user')
-
-    doc = Document.objects.create(
-        user=user,
-        name="My office doc",
-        state="IN_QUEUE",
-        file_type='.docx'
-    )
+    doc = create_doc("My office doc", ".docx")
 
     f = File(open('documents/tests/files/2pages.docx'))
     doc.original.save("silly-unique-deadbeef-file.docx", f)
@@ -110,3 +102,32 @@ def test_send_office():
     assert doc.page_set.count() == 2
     assert doc.page_set.count() == doc.pages
     assert doc.original.path != doc.pdf.path
+
+
+def test_correct_checksum():
+    doc = create_doc("Document name2", ".pdf")
+
+    f = File(open('documents/tests/files/3pages.pdf'))
+    doc.original.save("another-uuid-beef-dead.pdf", f)
+
+    result = tasks.checksum.delay(doc.id)
+    assert result.status == celery.states.SUCCESS, result.traceback
+
+    doc = Document.objects.get(id=doc.id) # Get back the updated instance
+    assert doc.md5 == "8be98044ac25f3050b121aceac618823"
+
+
+def test_duplicate_checksum():
+    doc = create_doc("Document name2", ".pdf")
+    doc.md5 = "8be98044ac25f3050b121aceac618823"
+    doc.save()
+
+    duplicate = create_doc("Document name2", ".pdf")
+
+    f = File(open('documents/tests/files/3pages.pdf'))
+    duplicate.original.save("another-uuid-beef-dead.pdf", f)
+
+    result = tasks.checksum.delay(duplicate.id)
+    assert result.status == celery.states.FAILURE
+    assert "ExisingChecksum" in result.traceback
+    assert Document.objects.filter(id=duplicate.id).count() == 0
