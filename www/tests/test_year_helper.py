d68e76bc8f4082aa205547fd9516ad2d24773629
Nikita Marchant <nikita.marchant@gmail.com>
2015-09-22 14:11:49 +0200

    Update www/tests/test_year_helper.py to be used with py.test

--- www/tests/test_year_helper.py
+++ www/tests/test_year_helper.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from www import helpers
 from datetime import datetime
 import mock
@@ -10,29 +13,33 @@ def mock_datetime(y, m, d, H, M, S):
     return res
 
 
-class TestClass:
-    Q1 = (2012, 12, 21, 11, 11, 11)
-    Q2 = (2013, 2, 12, 10, 10, 10)
-    Q2_2014 = (2015, 3, 19, 15, 35, 22)
+Q1 = (2012, 12, 21, 11, 11, 11)
+Q2 = (2013, 2, 12, 10, 10, 10)
+Q2_2014 = (2015, 3, 19, 15, 35, 22)
 
-    def testQ1(self):
-        helpers.datetime = mock_datetime(*self.Q1)
-        assert 2012 == helpers.current_year()
 
-    def testQ2(self):
-        helpers.datetime = mock_datetime(*self.Q2)
-        assert 2012 == helpers.current_year()
+def test_Q1():
+    helpers.datetime = mock_datetime(*Q1)
+    assert 2012 == helpers.current_year()
 
-    def testQ2_2014(self):
-        helpers.datetime = mock_datetime(*self.Q2_2014)
-        assert 2014 == helpers.current_year()
 
-    def testQ1List(self):
-        helpers.datetime = mock_datetime(*self.Q1)
-        expected = [("2012-2013",) * 2, ("Archives",) * 2]
-        assert expected == helpers.year_choices(backlog=1)
+def test_Q2():
+    helpers.datetime = mock_datetime(*Q2)
+    assert 2012 == helpers.current_year()
 
-    def testQ2List(self):
-        helpers.datetime = mock_datetime(*self.Q2)
-        expected = [("2012-2013",) * 2, ("Archives",) * 2]
-        assert expected == helpers.year_choices(backlog=1)
+
+def test_Q2_2014():
+    helpers.datetime = mock_datetime(*Q2_2014)
+    assert 2014 == helpers.current_year()
+
+
+def test_Q1List():
+    helpers.datetime = mock_datetime(*Q1)
+    expected = [("2012-2013",) * 2, ("Archives",) * 2]
+    assert expected == helpers.year_choices(backlog=1)
+
+
+def test_Q2List():
+    helpers.datetime = mock_datetime(*Q2)
+    expected = [("2012-2013",) * 2, ("Archives",) * 2]
+    assert expected == helpers.year_choices(backlog=1)
