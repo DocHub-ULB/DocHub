5f8b88049ca5e9e7140306153820c56bca0a703f
iTitou <moiandme@gmail.com>
2016-01-13 15:27:49 +0100

    [enh] Shorter

--- templates/catalog/course_react.js
+++ templates/catalog/course_react.js
@@ -1,35 +1,24 @@
 /* http://stackoverflow.com/questions/728360/most-elegant-way-to-clone-a-javascript-object */
-const clone = function(obj) {
+const clone = function(obj){
     var copy;
-
-    // Handle the 3 simple types, and null or undefined
     if (null == obj || "object" != typeof obj) return obj;
-
-    // Handle Date
-    if (obj instanceof Date) {
+    if (obj instanceof Date){
         copy = new Date();
         copy.setTime(obj.getTime());
         return copy;
     }
-
-    // Handle Array
-    if (obj instanceof Array) {
+    if (obj instanceof Array){
         copy = [];
-        for (var i = 0, len = obj.length; i < len; i++) {
-            copy[i] = clone(obj[i]);
-        }
+        for (var i=0, len=obj.length; i<len; i++){copy[i] = clone(obj[i]);}
         return copy;
     }
-
-    // Handle Object
-    if (obj instanceof Object) {
+    if (obj instanceof Object){
         copy = {};
-        for (var attr in obj) {
-            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        for (var attr in obj){
+            if (obj.hasOwnProperty(attr)){copy[attr] = clone(obj[attr]);}
         }
         return copy;
     }
-
     throw new Error("Unable to copy obj! Its type isn't supported.");
 }
 
