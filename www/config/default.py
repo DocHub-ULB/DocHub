a28155901b9e5917b4fc6c8fad05a130620b88c8
iTitou <moiandme@gmail.com>
2016-01-14 01:23:20 +0100

    [ref] Switch to sass

--- www/config/default.py
+++ www/config/default.py
@@ -109,50 +109,48 @@ PIWIK_SITE_ID = '1'
 
 
 PIPELINE = {
-    'COMPILERS': ('react.utils.pipeline.JSXCompiler', ),
-}
-
-PIPELINE['JAVASCRIPT'] = {
-    '3party': {
-        'source_filenames': (
-            '3party/foundation/js/vendor/modernizr.js',
-            '3party/jquery/jquery.js',
-            '3party/foundation/js/foundation.min.js',
-            '3party/markdown/markdown.js',
-            '3party/moment/moment-with-locales.js',
-            '3party/react/react.js',
-            '3party/react/react-dom.js',
-            '3party/mathjax/mathjax.js',
-            '3party/select/js/select2.js',
-            '3party/select/js/i18n/fr.js',
-            '3party/cookie/cookie.js'
-        ),
-        'output_filename': '3party.js',
+    'COMPILERS': ('react.utils.pipeline.JSXCompiler',
+                  'pipeline.compilers.sass.SASSCompiler',),
+    'JAVASCRIPT': {
+        '3party': {
+            'source_filenames': (
+                '3party/foundation/js/vendor/modernizr.js',
+                '3party/jquery/jquery.js',
+                '3party/foundation/js/foundation.min.js',
+                '3party/markdown/markdown.js',
+                '3party/moment/moment-with-locales.js',
+                '3party/react/react.js',
+                '3party/react/react-dom.js',
+                '3party/mathjax/mathjax.js',
+                '3party/select/js/select2.js',
+                '3party/select/js/i18n/fr.js',
+                '3party/cookie/cookie.js'
+            ),
+            'output_filename': '3party.js',
+        },
+        'main': {
+            'source_filenames': (
+                'scripts/viewer.js',
+                'scripts/*.jsx',
+            ),
+            'output_filename': 'main.js',
+        }
     },
-    'main': {
-        'source_filenames': (
-            'scripts/viewer.js',
-            'scripts/*.jsx',
-        ),
-        'output_filename': 'main.js',
+    'STYLESHEETS': {
+        '3party': {
+            'source_filenames': (
+                '3party/foundation/css/normalize.css',
+                '3party/foundation/css/foundation.css',
+                '3party/foundation-icons/foundation-icons.css',
+                '3party/select/css/select2.css',
+            ),
+            'output_filename': '3party.css',
+        },
+        'main': {
+            'source_filenames': (
+                'style/*.sass',
+            ),
+            'output_filename': 'main.css',
+        },
     }
-}
-
-PIPELINE['STYLESHEETS'] = {
-    '3party': {
-        'source_filenames': (
-            '3party/foundation/css/normalize.css',
-            '3party/foundation/css/foundation.css',
-            '3party/foundation-icons/foundation-icons.css',
-            '3party/select/css/select2.css',
-        ),
-        'output_filename': '3party.css',
-    },
-    'main': {
-        'source_filenames': (
-            'style/main.css',
-            'style/viewer.css',
-        ),
-        'output_filename': 'main.css',
-    },
 }
