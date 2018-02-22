3d80c7dfea385fb56503942e7a1fd01e074cbd01
Eligoan <themastereligoan@gmail.com>
2018-02-22 11:58:02 +0100

    Making the navbar sticky try 1

--- assets/viewer/navbar.jsx
+++ assets/viewer/navbar.jsx
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 
 const buttonStyle = {
-  "background-color":'rgba(51, 51, 51, 1)',
+  "backgroundColor":'rgba(51, 51, 51, 1)',
 }
 
 export default class Navbar extends Component {
   render() {
     return (
-        <div className="top-bar" id="navbar">
-            <a className="button left" href={"/catalog/course/" + this.props.couname} style={buttonStyle}>
-              <i className="fi-arrow-left"></i> {this.props.couname}
-            </a>
+        <div className="sticky">
+            <nav className="top-bar" id="navbar">
+                <a className="button left" href={"/catalog/course/" + this.props.couname} style={buttonStyle}>
+                  <i className="fi-arrow-left"></i> {this.props.couname}
+                </a>
 
-            <a className="button left" href={window.Urls.document_download(this.props.docid)} style={buttonStyle}>
-              <i className="fi-download"></i> Download
-            </a>
+                <a className="button left" href={window.Urls.document_download(this.props.docid)} style={buttonStyle}>
+                  <i className="fi-download"></i> Download
+                </a>
 
-            <font color="white">{this.props.docname}</font>
+                <font color="white">{this.props.docname}</font>
 
-            <button className="button right" onClick={this.props.zoomin} style={buttonStyle}>
-              <i className="fi-zoom-in"></i> Zoom
-            </button>
+                <button className="button right" onClick={this.props.zoomin} style={buttonStyle}>
+                  <i className="fi-zoom-in"></i> Zoom
+                </button>
 
-            <button className="button right" onClick={this.props.zoomout} style={buttonStyle}>
-              <i className="fi-zoom-out"></i> De-zoom
-            </button>
+                <button className="button right" onClick={this.props.zoomout} style={buttonStyle}>
+                  <i className="fi-zoom-out"></i> De-zoom
+                </button>
 
+            </nav>
         </div>
     );
   }
