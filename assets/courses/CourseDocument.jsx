dea1ad8a48854ff89b2b28dc9a17d64058bc980f
Pavel Polinetskyi <pavel.polinetskyi@gmail.com>
2018-03-01 17:37:14 +0100

    Refactor CourseDocument to ES6 class

--- assets/courses/CourseDocument.jsx
+++ assets/courses/CourseDocument.jsx
@@ -88,20 +88,24 @@ class DownvoteButton extends React.Component{
     }
 };
 
-const CourseDocument = React.createClass({
-    getInitialState: function(){
-        return {upvote_active: this.props.user_vote==1,
-                downvote_active: this.props.user_vote==-1,
-                upvotes: this.props.votes.upvotes,
-                downvotes: this.props.votes.downvotes};
-    },
-    ready: function(){return (this.props.is_ready);},
-    editable: function(){return this.props.has_perm;},
-    date: function(){return moment(this.props.date).format("D MMMM YYYY");},
-    edit_url: function(){return window.Urls.document_edit(this.props.id);},
-    reupload_url: function(){return window.Urls.document_reupload(this.props.id);},
-    url: function(){return window.Urls.document_show(this.props.id);},
-    icon: function(){
+class CourseDocument extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {upvote_active: this.props.user_vote==1,
+                      downvote_active: this.props.user_vote==-1,
+                      upvotes: this.props.votes.upvotes,
+                      downvotes: this.props.votes.downvotes
+        };
+        this.downvote_callback = this.downvote_callback.bind(this)
+        this.upvote_callback = this.upvote_callback.bind(this)
+    }
+    ready() {return (this.props.is_ready);}
+    editable() {return this.props.has_perm;}
+    date() {return moment(this.props.date).format("D MMMM YYYY");}
+    edit_url() {return window.Urls.document_edit(this.props.id);}
+    reupload_url() {return window.Urls.document_reupload(this.props.id);}
+    url() {return window.Urls.document_show(this.props.id);}
+    icon() {
         if (this.ready()){
             return (<a href={this.url()}>
                 <i className="fi-page-copy round-icon big"></i>
@@ -111,8 +115,8 @@ const CourseDocument = React.createClass({
         } else {
             return <i className="fi-save round-icon big"></i>;
         }
-    },
-    download_icon: function(){
+    }
+    download_icon() {
         if (this.ready()){
             var url = window.Urls.document_download(this.props.id)
         } else {
@@ -123,8 +127,8 @@ const CourseDocument = React.createClass({
                 <i className="fi-download dark-grey"></i> Télécharger
             </a>
         )
-    },
-    edit_icon: function(){
+    }
+    edit_icon() {
         if (this.ready() && this.editable()){
             return
             <a href={this.edit_url()} title="Éditer">
@@ -132,52 +136,52 @@ const CourseDocument = React.createClass({
             </a>;
         }
         return '';
-    },
-    reupload_icon: function(){
+    }
+    reupload_icon() {
         if (this.ready() && this.editable()){
             return (<a href={this.reupload_url()}>
                 <i className="fi-page-add dark-grey" title="Nouvelle version"></i> Ré-uploader
             </a>);
         }
-    },
-    upvote_icon: function(){
+    }
+    upvote_icon() {
         return (<UpvoteButton doc_id={this.props.id} num={this.state.upvotes} isActive={this.state.upvote_active} vote_callback={this.upvote_callback} />);
 
-    },
-    downvote_icon: function(){
+    }
+    downvote_icon() {
         return (<DownvoteButton doc_id={this.props.id} num={this.state.downvotes} isActive={this.state.downvote_active} vote_callback={this.downvote_callback}/>);
-    },
-    downvote_callback: function(response){
+    }
+    downvote_callback(response) {
         var new_downvotes = this.state.downvotes + 1
         var new_upvotes = this.state.upvotes - (response.created ? 0 : 1)
         this.setState({upvote_active: false,
                         downvote_active: true,
                         upvotes: new_upvotes,
                         downvotes: new_downvotes,});
-    },
-    upvote_callback: function(response){
+    }
+    upvote_callback(response) {
         var new_upvotes = this.state.upvotes + 1
         var new_downvotes = this.state.downvotes - (response.created ? 0 : 1)
         this.setState({upvote_active: true,
                         downvote_active: false,
                         upvotes: new_upvotes,
                         downvotes: new_downvotes,});
-    },
-    description: function(){
+    }
+    description() {
         var text = markdown.toHTML(this.props.description);
         if (text != ''){
             var wrap = {__html: text};
             return <p dangerouslySetInnerHTML={wrap} />;
         }
         return '';
-    },
-    title: function(){
+    }
+    title() {
         if (this.ready()){
             return <a href={this.url()}>{this.props.name}</a>;
         }
         return this.props.name;
-    },
-    pages: function(){
+    }
+    pages() {
         if (! this.ready()){
             return "En cours de traitement";
         }
@@ -188,13 +192,13 @@ const CourseDocument = React.createClass({
             return "";
         }
         return this.props.pages + " pages";
-    },
-    tags: function(){
+    }
+    tags() {
         return this.props.tags.map(function(tag){
             return <Tag key={"tag"+tag.id} {...tag}/>
         });
-    },
-    render: function(){
+    }
+    render() {
         return (<div className="row course-row document">
             {this.icon()} {this.upvote_icon()} {this.downvote_icon()}
             <div className="cell course-row-content">
@@ -212,6 +216,6 @@ const CourseDocument = React.createClass({
             </div>
         </div>);
     }
-});
+};
 
 export default CourseDocument;
