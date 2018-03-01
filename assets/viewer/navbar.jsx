1b7c247593777bb17a35fb493c19e80989fce790
Eligoan <themastereligoan@gmail.com>
2018-03-01 14:43:02 +0100

    [fix] permissions passing

--- assets/viewer/navbar.jsx
+++ assets/viewer/navbar.jsx
@@ -16,14 +16,16 @@ export default class Navbar extends Component {
                 {this.props.original != this.props.pdf?
                     (<a className="button navbar-button left" href={window.Urls.document_download_original(this.props.docid)}>
                       <i className="fi-download"></i> Télécharger - Original
-                    </a>):null}
+                    </a>)
+                    :null}
 
                 <font color="white">{this.props.docname}</font>
 
                 {this.props.has_perm?
                     (<a className="button navbar-button right" href={window.Urls.document_edit(this.props.docid)}>
                       <i className="fi-pencil"></i> Editer
-                    </a>):null}
+                    </a>)
+                    :null}
 
 
                 <button className="button navbar-button right" onClick={this.props.zoomin}>
