bbc5783e512129a7d5aa28e2011a0a9ab90f6d6c
Nikita Marchant <nikita.marchant@gmail.com>
2018-04-01 14:21:57 +0200

    Add doc to /api

--- www/rest_urls.py
+++ www/rest_urls.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 
 from rest_framework_extensions.routers import NestedRouterMixin
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, APIRootView
 
 import users.rest
 import documents.rest
@@ -12,11 +12,32 @@ import catalog.rest
 import telepathy.rest
 
 
-class SimpleRouterWithNesting(NestedRouterMixin, DefaultRouter):
+class DochubAPI(APIRootView):
+    """
+    This is the API of DocHub.
+
+    You are free to use it to crawl DocHub,
+    write your own frontend or even make a copy of our documents.
+
+    But please, if you do, respect those rules :
+
+     * To not hit the server too hard. If you degrade the service for other users, we will ban you.
+     * Respect the privacy of the users
+     * If you scrape and reuse our content, plase credit DocHub and the original uploader.
+
+    This whole API is auth protected.
+    To be able to use it without your session cookie,
+    use your personal token from <a hre="/api/me">/api/me</a>
+    ([doc](http://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication))
+    """
     pass
 
 
-router = SimpleRouterWithNesting()
+class Router(DefaultRouter):
+    APIRootView = DochubAPI
+
+
+router = Router()
 
 router.register(r'users', users.rest.UserViewSet)
 router.register(r'courses', catalog.rest.CourseViewSet)
