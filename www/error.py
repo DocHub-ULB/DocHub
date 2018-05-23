6da304d5a825a288a593e2f9e950118335e7e46d
Nikita Marchant <nikita.marchant@gmail.com>
2018-05-23 11:04:08 +0200

    Fix error pages

--- www/error.py
+++ www/error.py
@@ -4,17 +4,17 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 
-def error400(request):
+def error400(request, exception=None):
     return render(request, "error/400.html", status=400)
 
 
-def error403(request):
+def error403(request, exception=None):
     return render(request, "error/403.html", status=403)
 
 
-def error404(request):
+def error404(request, exception=None):
     return render(request, "error/404.html", status=404)
 
 
-def error500(request):
+def error500(request, exception=None):
     return render(request, "error/500.html", status=500)
