4d04e73db6b9d8c64ad34d962773061128b42f46
Nikita Marchant <nikita.marchant@gmail.com>
2018-05-28 18:11:58 +0200

    Remove tree

--- assets/tree/index.js
+++ assets/tree/index.js
@@ -1,12 +1,2 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const Menu = require('./menu.js');
-
-document.addEventListener("DOMContentLoaded",function(){
-    window.courseTreeRender = (data) => {
-        ReactDOM.render(
-            <Menu {...data[0]}/>,
-            document.getElementById('course-tree-menu')
-        );
-    };
-})
