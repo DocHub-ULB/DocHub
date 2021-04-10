1e8d5cd1fc76527c650d2e47794ef3af3992dea7
Nikita Marchant <nikita.marchant@gmail.com>
2021-04-10 19:22:43 +0200

    Fix broken test 😑

--- users/tests/auth_backend_authenticate_test.py
+++ users/tests/auth_backend_authenticate_test.py
@@ -1,3 +1,5 @@
+from django.test.client import RequestFactory
+
 import pytest
 import responses
 
@@ -10,21 +12,23 @@ pytestmark = pytest.mark.django_db
 @responses.activate
 def test_auth():
     sid = "this-is-a-sid"
-    uid = 'this-is-a-uid-and-is-longer'
+    uid = "this-is-a-uid-and-is-longer"
     xml = open("users/tests/xml-fixtures/nimarcha.xml").read()
 
     responses.add(
         responses.GET,
-        f'https://www.ulb.ac.be/commons/check?_type=normal&_sid={sid}&_uid={uid}',
-        body=xml, status=200,
-        match_querystring=True
+        f"https://www.ulb.ac.be/commons/check?_type=normal&_sid={sid}&_uid={uid}",
+        body=xml,
+        status=200,
+        match_querystring=True,
     )
 
-    user = NetidBackend().authenticate(sid=sid, uid=uid)
+    rf = RequestFactory()
+    user = NetidBackend().authenticate(rf.get("/does-not-matter"), sid=sid, uid=uid)
 
     assert len(responses.calls) == 1
     assert isinstance(user, User)
-    assert user.netid == 'nimarcha'
-    assert User.objects.filter(netid='nimarcha').count() == 1
+    assert user.netid == "nimarcha"
+    assert User.objects.filter(netid="nimarcha").count() == 1
     assert open(f"/tmp/netids/{sid}__{uid}").read() == xml
     assert user.inscription_set.count() == 4
