ee3bee3723b8c17d3e22f7783607a819763f2b6a
Nikita Marchant <nikita.marchant@gmail.com>
2021-04-12 17:44:11 +0200

    Fix cleanup

--- www/config/default.py
+++ www/config/default.py
@@ -15,24 +15,24 @@ from www.config.django_defaults import *
 EMAIL_SUBJECT_PREFIX = "[DocHub] "
 
 # User Profile Model
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = "users.User"
 
 # Page to show after a syslogin
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = "index"
 
 # url to internal login
-LOGIN_URL = '/'
+LOGIN_URL = "/"
 
-UPLOAD_DIR = join(MEDIA_ROOT, 'documents')
+UPLOAD_DIR = join(MEDIA_ROOT, "documents")
 
-DOCUMENT_STORAGE = 'django.core.files.storage.FileSystemStorage'
+DOCUMENT_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 BASE_URL = "https://dochub.be/"
 
 # CELERY ---
 
 task_serializer = "json"
-accept_content = ['json', 'msgpack']
+accept_content = ["json", "msgpack"]
 worker_prefetch_multiplier = 1  # Do not prefetch more than 1 task
 
 # END CELERY ----
@@ -40,90 +40,77 @@ worker_prefetch_multiplier = 1  # Do not prefetch more than 1 task
 # Activate identicons
 IDENTICON = True
 
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'scripts/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
-    }
-}
 
 # libs
 INSTALLED_APPS += (
-    'django.contrib.humanize',
-    'django.contrib.admin',
-    'django.contrib.sites', # needed for sitemap
-    'django.contrib.sitemaps',
-    'rest_framework',
-    'mptt',
-    'django_js_reverse',
-    'webpack_loader',
-    'rest_framework.authtoken',
-    'django.contrib.postgres',
-    'sass_processor'
+    "django.contrib.humanize",
+    "django.contrib.admin",
+    "django.contrib.sites",  # needed for sitemap
+    "django.contrib.sitemaps",
+    "rest_framework",
+    "mptt",
+    "rest_framework.authtoken",
+    "django.contrib.postgres",
+    "sass_processor",
 )
 
 SITE_ID = 1
 
 # apps
 INSTALLED_APPS += (
-    'www',
-    'documents',
-    'telepathy',
-    'users',
-    'catalog',
-    'tags',
-    'notifications',
-    'search'
+    "www",
+    "documents",
+    "telepathy",
+    "users",
+    "catalog",
+    "tags",
+    "notifications",
+    "search",
 )
 
 # must be after everything
-INSTALLED_APPS += (
-    'actstream',
+INSTALLED_APPS += ("actstream",)
+
+TEMPLATES[0]["OPTIONS"]["context_processors"] += (  # NOQA
+    "django.template.context_processors.request",
+    "www.context_processors.read_only",
 )
 
-TEMPLATES[0]['OPTIONS']['context_processors'] += ( # NOQA
-    'django.template.context_processors.request',
-    'www.context_processors.raven',
-    'www.context_processors.read_only',
-)
-
-STATIC_ROOT = 'collected_static'
+STATIC_ROOT = "collected_static"
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'sass_processor.finders.CssFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "sass_processor.finders.CssFinder",
 )
 
-SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, "static")
 
 
 AUTHENTICATION_BACKENDS = (
-    'users.authBackend.NetidBackend',
-    'django.contrib.auth.backends.ModelBackend',
+    "users.authBackend.NetidBackend",
+    "django.contrib.auth.backends.ModelBackend",
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'www.rest_renderers.VaryBrowsableAPIRenderer',
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "www.rest_renderers.VaryBrowsableAPIRenderer",
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20,
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    )
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 20,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ),
 }
 
 ACTSTREAM_SETTINGS = {
-    'USE_JSONFIELD': True,
+    "USE_JSONFIELD": True,
 }
-
-JS_REVERSE_EXCLUDE_NAMESPACES = ['admin', 'djdt']
 
 MAX_RENDER_PAGES = 100
 
@@ -131,4 +118,4 @@ READ_ONLY = False
 
 REJECTED_FILE_FORMATS = (".zip", ".tar", ".gz", ".rar")
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = "SAMEORIGIN"
