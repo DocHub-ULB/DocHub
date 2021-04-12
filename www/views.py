01bc34c5c2efad5d5fd94a493d722be90b0a7715
Nikita Marchant <nikita.marchant@gmail.com>
2021-04-12 17:40:16 +0200

    Cleanup

--- www/views.py
+++ www/views.py
@@ -18,24 +18,27 @@ def index(request):
     if request.user.is_authenticated:
         following = request.user.following_courses()
         ndocs = max(5, len(following))
-        docs = Document.objects.filter(course__in=following).order_by("-created")[:ndocs]
+        docs = Document.objects.filter(course__in=following).order_by("-created")[
+            :ndocs
+        ]
         context = {
-            'search': SearchForm(),
-            'stream': user_stream(request.user).exclude(verb="started following")[:10],
-            'recent_docs': docs,
-            'faculties': Category.objects.get(level=0).children.all()
+            "search": SearchForm(),
+            "stream": user_stream(request.user).exclude(verb="started following")[:10],
+            "recent_docs": docs,
+            "faculties": Category.objects.get(level=0).children.all(),
         }
         return render(request, "home.html", context)
     else:
+
         def floor(num, r=1):
             r = 10 ** r
             return int((num // r) * r) if r != 0 else 0
 
         if Document.objects.count():
-            page_count = Document.objects.all().aggregate(Sum('pages'))['pages__sum']
+            page_count = Document.objects.all().aggregate(Sum("pages"))["pages__sum"]
         else:
             page_count = 0
-            
+
         context = {
             "login_url": NetidBackend.login_url(""),
             "debug": settings.DEBUG,
