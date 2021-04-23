1e3a5e82edf2a0d5d8c6ac1ada01cd49612778a7
Nikita Marchant <nikita.marchant@gmail.com>
2021-04-23 17:48:55 +0200

    Fix tests

--- users/tests/auth_backend_authenticate_test.py
+++ users/tests/auth_backend_authenticate_test.py
@@ -9,26 +9,28 @@ from users.models import User
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture()
+def fake_base_url(settings):
+    settings.BASE_URL = "http://example.com/"
+
+
 @responses.activate
-def test_auth():
-    sid = "this-is-a-sid"
-    uid = "this-is-a-uid-and-is-longer"
-    xml = open("users/tests/xml-fixtures/nimarcha.xml").read()
+def test_auth(fake_base_url):
+    ticket = "this-is-a-cas-ticket"
+    xml = open("users/tests/xml-fixtures/minimal.xml").read()
 
     responses.add(
         responses.GET,
-        f"https://www.ulb.ac.be/commons/check?_type=normal&_sid={sid}&_uid={uid}",
+        f"https://auth-pp.ulb.be/proxyValidate?ticket={ticket}&service=http%3A%2F%2Fexample.com%2Fauth-ulb",
         body=xml,
         status=200,
         match_querystring=True,
     )
 
     rf = RequestFactory()
-    user = NetidBackend().authenticate(rf.get("/does-not-matter"), sid=sid, uid=uid)
+    user = NetidBackend().authenticate(rf.get("/this-is-a-fake-django-request"), ticket=ticket)
 
     assert len(responses.calls) == 1
     assert isinstance(user, User)
-    assert user.netid == "nimarcha"
-    assert User.objects.filter(netid="nimarcha").count() == 1
-    assert open(f"/tmp/netids/{sid}__{uid}").read() == xml
-    assert user.inscription_set.count() == 4
+    assert user.netid == "nmar0003"
+    assert User.objects.filter(netid="nmar0003").count() == 1
