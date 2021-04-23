8e866cc3dfdcc97e4d7ccb2f89e518a0e86aacc1
Nikita Marchant <nikita.marchant@gmail.com>
2021-04-23 18:59:18 +0200

    Add tests from real data

--- users/tests/auth_backend_parser_real_life.py
+++ users/tests/auth_backend_parser_real_life.py
@@ -2,16 +2,16 @@
 
 import glob
 
+import pytest
+
 from users.authBackend import IntranetError, NetidBackend
 
 parse = NetidBackend()._parse_response
 
 
-def test_parser_real_life():
-    for fpath in glob.glob("users/tests/xml-private-data/*.xml"):
-        xml = open(fpath).read()
-        try:
-            ret = parse(xml)
-            assert ret is not None
-        except IntranetError:
-            pass
+@pytest.mark.parametrize("fpath", glob.glob("users/tests/xml-private-data/*.xml") + glob.glob("users/tests/xml-anonymized-data/*.xml"))
+def test_parser_real_life(fpath):
+    with open(fpath) as fd:
+        xml = fd.read()
+        ret = parse(xml)
+        assert ret is not None
