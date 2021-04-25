0167f72b872c12a544b6a7303510fc9f24c62dd1
Nikita Marchant <nikita.marchant@gmail.com>
2021-04-25 23:15:12 +0200

    Fix import error (thanks mypy !)

--- users/tests/auth_backend_parser_real_life.py
+++ users/tests/auth_backend_parser_real_life.py
@@ -4,14 +4,12 @@ import glob
 
 import pytest
 
-from users.authBackend import IntranetError, UlbCasBackend
-
-parse = UlbCasBackend()._parse_response
+from users.authBackend import UlbCasBackend
 
 
 @pytest.mark.parametrize("fpath", glob.glob("users/tests/xml-private-data/*.xml") + glob.glob("users/tests/xml-anonymized-data/*.xml"))
 def test_parser_real_life(fpath):
     with open(fpath) as fd:
         xml = fd.read()
-        ret = parse(xml)
+        ret = UlbCasBackend()._parse_response(xml)
         assert ret is not None
