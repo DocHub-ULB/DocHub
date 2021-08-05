3c20cd492063f37b1713b2e47ac4002ffdae0a23
Minigrim0 <grimauflorent@gmail.com>
2021-08-05 23:43:35 +0200

    [Add] program list building

--- www/utils.py
+++ www/utils.py
@@ -13,3 +13,37 @@ def get_env(secret_name: str, default: Optional[str] = None):
     if result is None:
         return default
     return result
+
+
+def programType(program) -> str:
+    """Returns the type of the program (Bachelier, Master, CAP, ...)"""
+    # FIXME : Maybe find a more elegant solution to define a program's type ?
+    if "bachelier" in program.name.lower():
+        return "Bachelier"
+    if "master de spécialisation" in program.name.lower():
+        return "Master de spécialisation"
+    if "master" in program.name.lower():
+        return "Master"
+    if "certificat" in program.name.lower():
+        return "Certificat"
+    if "agrégation" in program.name.lower():
+        return "Agrégation"
+    else:
+        return "Autre"
+
+
+def buildProgramDict(programs) -> list:
+    """Builds a list of program types (Bachelier, Master, CAP) with the corresping programs in it"""
+    program_dict: dict = {}
+
+    for program in programs:
+        program_type = programType(program)
+        if program_type not in program_dict.keys():
+            program_dict[program_type] = {
+                "name": program_type,
+                "programs": [program]
+            }
+        else:
+            program_dict[program_type]['programs'].append(program)
+
+    return [program for _, program in program_dict.items()]
