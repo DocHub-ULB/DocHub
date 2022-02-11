6b1730bf0c17ce1b0d247be7f03ea7964cae4b8e
Nikita Marchant <nikita.marchant@gmail.com>
2022-02-11 19:17:18 +0300

    POC

--- static/main.js
+++ static/main.js
@@ -3,8 +3,9 @@ import _ from 'https://cdn.skypack.dev/lodash';
 import hotwiredTurbo from 'https://cdn.skypack.dev/@hotwired/turbo';
 import {Controller, Application} from 'https://cdn.skypack.dev/@hotwired/stimulus';
 
-const application = Application.start()
-
+import bundledEsModulesPdfjsDist from 'https://cdn.skypack.dev/@bundled-es-modules/pdfjs-dist@2.5.207-rc1';
+let pdfjs = bundledEsModulesPdfjsDist
+pdfjs.GlobalWorkerOptions.workerSrc = "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.5.207/build/pdf.worker.js"
 
 class CourseFilter extends Controller {
     static targets = [ "query", "tag", "filterable" ]
@@ -38,5 +39,56 @@ class Search extends Controller {
     }
 }
 
+
+class Viewer extends Controller {
+    static targets = ["renderer"]
+
+    initialize() {
+
+        var loadingTask = pdfjs.getDocument("http://localhost:8000/api/documents/4/pdf/?embed");
+        (async function() {
+            var pdf = await loadingTask.promise;
+            // Fetch the first page.
+            var page = await pdf.getPage(1);
+            var scale = 0.9;
+            var viewport = page.getViewport({ scale: scale, });
+            // Support HiDPI-screens.
+            var outputScale = window.devicePixelRatio || 1;
+
+            // Prepare canvas using PDF page dimensions.
+            var canvas = document.getElementById('the-canvas');
+            var context = canvas.getContext('2d');
+
+            canvas.width = Math.floor(viewport.width * outputScale);
+            canvas.height = Math.floor(viewport.height * outputScale);
+            canvas.style.width = Math.floor(viewport.width) + "px";
+            canvas.style.height =  Math.floor(viewport.height) + "px";
+
+            var transform = outputScale !== 1
+                ? [outputScale, 0, 0, outputScale, 0, 0]
+                : null;
+
+            // Render PDF page into canvas context.
+            var renderContext = {
+                canvasContext: context,
+                transform,
+                viewport,
+            };
+            page.render(renderContext);
+        })();
+    }
+
+}
+
+
+
+
+
+
+
+const application = Application.start()
+
 application.register("course-filter", CourseFilter);
 application.register("search", Search);
+application.register("viewer", Viewer);
+application.debug = true;
