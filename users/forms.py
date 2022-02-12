47542eb55d56f98331fed1387461215964f35d23
morti <qstevens@hotmail.be>
2022-02-12 16:23:24 +0100

    [ADD] custom for easily add moderated course to an user

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+from catalog.models import Course
+from users.models import User
+
 
 class SettingsForm(forms.Form):
     profile_pic = forms.ImageField()
+
+
+class UserModeratedCourseForm(forms.Form):
+    user = forms.ModelChoiceField(queryset=User.objects.all())
+    courses = forms.ModelMultipleChoiceField(queryset=Course.objects.all())
