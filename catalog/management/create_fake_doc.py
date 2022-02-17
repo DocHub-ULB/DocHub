03e0c5bcebcbac4af85af8c92c89ce5bd78e0382
morti <43477020+Mortinat@users.noreply.github.com>
2022-02-17 14:49:56 +0100

    [FIX] create_fake_doc (#229)

--- catalog/management/create_fake_doc.py
+++ catalog/management/create_fake_doc.py
@@ -9,15 +9,15 @@ from documents.models import Document
 from tags.models import Tag
 from users.models import User
 
-Tag.objects.create(name="officiel")
-Tag.objects.create(name="examen")
-Tag.objects.create(name="resume")
-Tag.objects.create(name="synthese")
-Tag.objects.create(name="notes")
+Tag.objects.get_or_create(name="officiel")
+Tag.objects.get_or_create(name="examen")
+Tag.objects.get_or_create(name="resume")
+Tag.objects.get_or_create(name="synthese")
+Tag.objects.get_or_create(name="notes")
 
 
 for course in Course.objects.all():
-    for i in range(16):
+    for i in range(5):
         doc = Document.objects.create(
             name=" ".join(generate()),
             course=course,
