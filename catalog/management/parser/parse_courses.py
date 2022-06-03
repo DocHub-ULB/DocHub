122f3bb26efa0900c81dd51b731d4a9c27181227
Nikita Marchant <nikita.marchant@gmail.com>
2022-06-03 18:39:10 +0200

    Cleanup

--- catalog/management/parser/parse_courses.py
+++
@@ -1,9 +0,0 @@
-import json
-import re
-import time
-from urllib.parse import quote, urlencode
-
-import requests
-from bs4 import BeautifulSoup
-from rich import print
-from rich.progress import MofNCompleteColumn, Progress, SpinnerColumn
