1de650cf9feb6629d666668aede68c4403845bd4
Nikita Marchant <nikita.marchant@gmail.com>
2022-06-03 18:39:10 +0200

    Cleanup

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 import users.views
 
 urlpatterns = [
-    path("reset_token/", users.views.reset_token, name="reset_token"),
     path("panel_hide/", users.views.panel_hide, name="hide_new_panel"),
 ]
