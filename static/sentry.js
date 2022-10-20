fbd95ea5feaef6d37b118cde73f65b217423b3d3
0xC4 <nikita.marchant@gmail.com>
2022-10-20 13:26:26 +0200

    Do not try to initialize sentry if we have no DSN (#254)

--- static/sentry.js
+++ static/sentry.js
@@ -4,10 +4,12 @@ import { BrowserTracing } from "https://cdn.skypack.dev/@sentry/tracing";
 const head = document.querySelector("head");
 const DSN = head.dataset.sentryDsn;
 
-Sentry.init({
-    dsn: DSN,
-    integrations: [new BrowserTracing()],
-    release: head.dataset.sentryRelease,
-});
+if (DSN.length > 0 && DSN.startsWith("https://")) {
+    Sentry.init({
+        dsn: DSN,
+        integrations: [new BrowserTracing()],
+        release: head.dataset.sentryRelease,
+    });
 
-Sentry.setUser({ email: head.dataset.sentryEmail });
+    Sentry.setUser({ email: head.dataset.sentryEmail });
+}
