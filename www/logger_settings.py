27b910d1b3ef5ef466f1032f14cea79943a431a0
Amrutha <amruthar1098@gmail.com>
2022-11-11 22:45:38 +0530

    fixing the pre commit changes for logger settings

--- www/logger_settings.py
+++ www/logger_settings.py
@@ -3,12 +3,12 @@ import sys
 
 logger = logging.getLogger()
 
-# specify here to change log level 
-log_level=logging.DEBUG
+# specify here to change log level
+log_level = logging.DEBUG
 logger.setLevel(log_level)
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
