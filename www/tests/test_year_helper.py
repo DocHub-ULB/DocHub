e679e4d264ca820c5414fa5aab48b1fee017e059
Nikita Marchant <nikita.marchant@gmail.com>
2023-03-18 22:47:31 +0100

    Use time-machine instead of shitty mock

--- www/tests/test_year_helper.py
+++ www/tests/test_year_helper.py
@@ -1,45 +1,37 @@
 from datetime import datetime
-from unittest import mock
 
+import time_machine
 from pytz import UTC
 
 from www import helpers
 
-
-def mock_datetime(y, m, d, H, M, S):
-    res = mock.MagicMock()
-    res.today = mock.Mock(return_value=datetime(y, m, d, tzinfo=UTC))
-    res.now = mock.Mock(return_value=datetime(y, m, d, H, M, S, tzinfo=UTC))
-    return res
+Q1 = datetime(2012, 12, 21, 11, 11, 11, tzinfo=UTC)
+Q2 = datetime(2013, 2, 12, 10, 10, 10, tzinfo=UTC)
+Q2_2014 = datetime(2015, 3, 19, 15, 35, 22, tzinfo=UTC)
 
 
-Q1 = (2012, 12, 21, 11, 11, 11)
-Q2 = (2013, 2, 12, 10, 10, 10)
-Q2_2014 = (2015, 3, 19, 15, 35, 22)
-
-
+@time_machine.travel(Q1)
 def test_Q1():
-    helpers.datetime = mock_datetime(*Q1)
     assert helpers.current_year() == 2012
 
 
+@time_machine.travel(Q2)
 def test_Q2():
-    helpers.datetime = mock_datetime(*Q2)
     assert helpers.current_year() == 2012
 
 
+@time_machine.travel(Q2_2014)
 def test_Q2_2014():
-    helpers.datetime = mock_datetime(*Q2_2014)
     assert helpers.current_year() == 2014
 
 
+@time_machine.travel(Q1)
 def test_Q1List():
-    helpers.datetime = mock_datetime(*Q1)
     expected = [("2012-2013",) * 2, ("Archives",) * 2]
     assert expected == helpers.year_choices(backlog=1)
 
 
+@time_machine.travel(Q2)
 def test_Q2List():
-    helpers.datetime = mock_datetime(*Q2)
     expected = [("2012-2013",) * 2, ("Archives",) * 2]
     assert expected == helpers.year_choices(backlog=1)
