52d265dd0397a3e43b8f24fd654209c52ea21efa
Nikita Marchant <nikita.marchant@gmail.com>
2025-09-23 21:50:43 +0200

    Upgrade python dependencies

--- documents/tests/thumbnail_test.py
+++ documents/tests/thumbnail_test.py
@@ -9,15 +9,15 @@ def test_single_page():
 
 def test_single_page_bytes():
     with open("documents/tests/files/1page.pdf", "rb") as fp:
-        i, img = get_thumbnail(stream=fp)
+        i, _img = get_thumbnail(stream=fp)
     assert i == 0
 
 
 def test_2page_first_almost_empty_takes_second():
-    i, img = get_thumbnail("documents/tests/files/2page_first_almost_empty.pdf")
+    i, _img = get_thumbnail("documents/tests/files/2page_first_almost_empty.pdf")
     assert i == 1
 
 
 def test_all_almost_empty_takes_first():
-    i, img = get_thumbnail("documents/tests/files/repaired.pdf")
+    i, _img = get_thumbnail("documents/tests/files/repaired.pdf")
     assert i == 0
