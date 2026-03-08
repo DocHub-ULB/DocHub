09d0aab4dd82a1218d9f5ccd502e17f6bfa879c3
mnietona <matias.nieto@student.uclouvain.be>
2026-03-08 21:07:58 +0100

    feat : migrate clean_archives command to logging

--- catalog/management/commands/clean_archives.py
+++ catalog/management/commands/clean_archives.py
@@ -1,18 +1,22 @@
 from typing import Any
 
+import logging
+
 from django.core.management.base import BaseCommand
 from django.db.models import Count
 
 from catalog.models import Course
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> None:
-        print("Cleaning archives")
+        logger.info("Cleaning archives")
         empty_archived_courses = (
             Course.objects.filter(is_archive=True)
             .annotate(num_doc=Count("document"))
             .filter(num_doc=0)
         )
-        print("Deleting %s empty courses" % len(empty_archived_courses))
+        logger.info("Deleting %s empty courses", len(empty_archived_courses))
         empty_archived_courses.delete()
