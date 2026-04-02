4d6ffb904e33409a2b180536b00296acc7fda44c
mnietona <matias.nieto@student.uclouvain.be>
2026-04-02 10:42:02 +0200

    chore(admin): display readable target object in ModerationLog admin

--- moderation/admin.py
+++ moderation/admin.py
@@ -14,7 +14,7 @@ class ModerationLogAdmin(admin.ModelAdmin):
     list_display = (
         "user",
         "content_type",
-        "object_id",
+        "target_item",
         "target_field",
         "old_value",
         "new_value",
@@ -22,3 +22,10 @@ class ModerationLogAdmin(admin.ModelAdmin):
     )
     list_filter = ("target_field", "timestamp", "content_type")
     search_fields = ("user__netid", "object_id", "old_value", "new_value")
+
+    @admin.display(description="Objet ciblé")
+    def target_item(self, obj):
+        """Retourne le nom lisible de l'objet ciblé au lieu de son simple numéro d'ID"""
+        if obj.content_object:
+            return str(obj.content_object)
+        return f"ID {obj.object_id} (Supprimé)"
